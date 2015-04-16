8314de0898610449cbcf7cf003de171937b4d7e9
tylorspence <tylor.spence@conceptshare.com>
2015-04-15 22:22:41 -0400

    menu setup

---
+++ app/menu/menu.directive.js
@@ -0,0 +1,78 @@
+angular
+	.module('menuModule', [])
+    .directive('menu', menu);
+
+	menu.$inject = [];
+
+	function menu() {
+		var directive = {
+			restrict: 'C',
+			templateUrl: 'app/menu/menu.html',
+			replace: true,
+			controller: menuController,
+			bindToController: true
+		};
+		return directive;
+	}
+
+	menuController.$inject = ['$scope', '$timeout', 'themeService', 'discService', 'eventService', 'visualizerService', 'SYNTHS', 'MENU_SIZE'];
+
+    function menuController($scope, $timeout,themeService,discService,eventService,visualizerService, SYNTHS, MENU_SIZE)	{
+
+        $scope.themeService = themeService;
+        $scope.eventService = eventService;
+        $scope.visualizerService = visualizerService;
+        $scope.discService = discService;
+        $scope.menuSize = MENU_SIZE;
+
+        $scope.editingVol = false;
+        $scope.editingSpd = false;
+        $scope.editingLen = false;
+
+
+        $scope.updateLen = {
+            toRun: function(x) {
+                discService.playing = false;
+                discService.discLength = 4 + Math.floor(x*28);
+                discService.reCalculateDiscs();
+            }
+        };
+        $scope.updateLen.toRun(discService.len);
+
+        $scope.changeVisualizer = function(index) {
+            visualizerService.clearCanvas();
+            visualizerService.visualizerIndex = index;
+        };
+
+
+        $scope.helpWindow = function() {
+            $scope.importExportVisible = false;
+            $scope.helpWindowVisible = !$scope.helpWindowVisible;
+        };
+        $scope.importExportWindow = function() {
+            $scope.helpWindowVisible = false;
+            $scope.importExportVisible = !$scope.importExportVisible;
+            var data = JSON.stringify(getStorageInfo(discService,themeService,visualizerService));
+            $root$scope.$broadcast("importExport",data);
+        };
+
+        $scope.changeTheme = function(item) {
+            themeService.theme = item;
+            discService.reCalculateDiscs();
+            $root$scope.$broadcast("themeChangeEvent");
+
+        };
+
+        $scope.changeSynth = function(index) {
+            discService.switchSynthTemplate(index);
+        };
+
+        $scope.resetSynth = function(index) {
+            discService.synthTemplates[index] = angular.copy(SYNTHS[index]);
+            $scope.resetIndex = index;
+            $timeout(function() {
+                $scope.resetIndex = -1;
+            },400)
+        }
+
+    }
---
+++ app/menu/synthControls/synthControls.directive.js
@@ -0,0 +1,30 @@
+angular
+	.module('synthControlsModule', [])
+    .directive('synthControls', synthControls);
+
+	synthControls.$inject = [];
+
+	function synthControls() {
+		var directive = {
+			restrict: 'C',
+			templateUrl: 'app/menu/synthControls/synthControls.html',
+			replace: true,
+			controller: synthControlsController,
+			bindToController: true
+		};
+		return directive
+	}
+
+	synthControlsController.$inject = ['$scope', 'discService'];
+
+	function synthControlsController($scope, discService) {
+
+		$scope.resetIndex = -1;
+		$scope.discService = discService;
+		$scope.oscWaveTypes = [
+			{txt: 'Sine', type: 'sine'},
+			{txt: 'Square', type: 'square'},
+			{txt: 'SawTooth', type: 'sawtooth'},
+			{txt: 'Triangle', type: 'triangle'}
+		];
+	}
