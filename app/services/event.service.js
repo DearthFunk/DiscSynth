3a5ba489f996bbf8db02a225b33dfc3c3701fcba
tylorspence <tylor.spence@conceptshare.com>
2015-04-21 19:07:55 -0400

    event service refactor

--- app/services/event.service.js
+++ app/services/event.service.js
@@ -1,15 +1,20 @@
 angular.module('eventServiceModule', [])
 
-    .service("eventService", function($window){
-        var eventServiceScope = this;
-        eventServiceScope.events = {
-	        mouseX: 0,
-	        mouseY: 0,
-	        angle: 0,
-	        mouseDown: false,
-	        keyFx1: false,
-	        keyFx2: false,
-	        keySquare: false
-        };
-    });
+    .factory("eventService", eventService);
 
+	eventService.$inject = [];
+
+	function eventService(){
+        var service = {
+	        events: {
+		        mouseX: 0,
+		        mouseY: 0,
+		        angle: 0,
+		        mouseDown: false,
+		        keyFx1: false,
+		        keyFx2: false,
+		        keySquare: false
+	        }
+        };
+		return service;
+    }
